154deb5b3627e7924ba652ba39bc568792549559
daniel <bro.danielx@gmail.com>
2019-09-18 06:06:20 -0400

    clean spreadsheet.py

---
+++ plot_individual.py
@@ -0,0 +1,16 @@
+'''
+Todo:
+- get concatenated dataframe
+- get last row using iloc 
+- get index => should be list of names
+- get dates
+- loop through list of names, get_series_by_column_title, plot
+
+
+spreadsheet: possible functions to delete:
+- get_google_sheet
+- get_data_frame_from_google_sheet
+- plot_data
+- plot_column
+- get_plots_from_workbook
+'''
--- spreadsheet.py
+++ spreadsheet.py
@@ -79,70 +79,7 @@ def clean_workbook_data(data, workbook_title):
         return data.loc[data.index.dayofweek == 0]
     return data
 
-
-
-def get_google_sheet(workbook_name):
-    """ 
-    Returns all records of first sheet of workbook in the form of a 
-    list of dictionaries 
-    """
-
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        CLIENT_SECRET_FILENAME, SCOPE
-        )
-    client = gspread.authorize(credentials)
-
-    # sheet = client.open(workbook_name).sheet1
-    sheet = client.open(workbook_name).get_worksheet(2)
-    data = sheet.get_all_records()
-    sheet_title = sheet.title
-    print(sheet_title)
-
-    return data
-
-
-def get_data_frame_from_google_sheet(workbook_name):
-    data_list = get_google_sheet(workbook_name)
-    data = pd.DataFrame(data_list)
-    data.set_index('Week', inplace=True)
-    return clean_data(data)
-
 def clean_data(data):
     data = data.replace(r'', 0)
     data.index = pd.to_datetime(data.index)
     return data
-
-
-def plot_data(data):
-    for col in data.columns:
-        plot_column(data, col)
-
-def plot_column(data, column='Total', workbook_name=''):
-    """ Plot DataFrame """
-    fig = plt.figure(figsize=(15,8))
-    ax = fig.add_subplot(111)
-    ax.plot(data.index, data[column])
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%b \'%y'))
-
-    plt.title(f'{column} FCN')
-    plt.xlabel('Date')
-    plt.ylabel('FCN')
-    # plt.style.use('seaborn-deep')
-
-    plt.show()
-    # plt.savefig(f'{column}_{workbook_name}.png', bbox_inches='tight')
-
-
-def get_plots_from_workbook(workbook_name):
-    data = get_data_frame_from_google_sheet(workbook_name)
-    # pprint(data.head())
-    # plot_data(data)
-    plot_column(data, 'Total', workbook_name)
-
-
-
-if __name__ == '__main__':
-    # get_plots_from_workbook(FCN_WORKBOOK_NAME)
-    workbooks = get_google_workbooks(WORKBOOK_NAME_DICTIONARIES)
-    print(len(workbooks))
-    # pprint(workbooks[0]['data'], indent=2)
